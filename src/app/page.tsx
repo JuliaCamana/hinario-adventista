dc7b3c3a0cb09cbd355c33058686527e558a7bc9
Julia Camana <juliacamana.2017@gmail.com>
2025-06-23 01:49:28 +0200

    tela inicial em desenvolvimento 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { ContentSearchHinos, ContentSearchListHinos } from "./styles";
 import { FaSearch } from "react-icons/fa";
 
