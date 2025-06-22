e6ab41e25f1872bdca2aa3199cf73ac8ceae0c30
Julia Camana <juliacamana.2017@gmail.com>
2025-06-23 01:43:02 +0200

    tela inicial em desensenvolvimento 0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,51 @@
+'use client'
 import React from "react";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { ContentSearchHinos, ContentSearchListHinos } from "./styles";
+import { FaSearch } from "react-icons/fa";
+
 
 export default function Home() {
   return (
- 
-      <p style={{textAlign: "center", margin:"200px", fontSize:"70px"}}>Estamos em construção</p>
+    <div>
+      <nav className="navbar bg-body-tertiary ">
+        <div className="container-fluid justify-content-center ">
+          <span className="navbar-brand mb-0 h1 ">Hinario Aventista</span>
+        </div>
+      </nav>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end"  id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#">Sobre</a>
+              <a className="nav-link" href="#">Contacto</a>
+              <a className="nav-link" href="#">Credito</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <ContentSearchHinos>
+
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Procurar por titulo ou numero" aria-label="searchHino" aria-describedby="basic-addon1" />
+        <span className="input-group-text" id="basic-addon1"><button type="button" className="btn"><FaSearch/></button></span>
+      </div>
+      <ContentSearchListHinos>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">1 . oh Deus de amor </li>
+        <li className="list-group-item">2 . Quao Grande es tu</li>
+        <li className="list-group-item">3 . Rocha Eterna</li>
+        <li className="list-group-item">4. A ultima hora</li>
+        <li className="list-group-item">5. Bendita Seguranca</li>
+      </ul>
+      </ContentSearchListHinos>
+      </ContentSearchHinos>
+
+    </div>
+
   );
 }
---
+++ src/app/styles.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+
+export const Button = styled.button`
+
+
+`
+export const Menu = styled.nav`
+
+
+`
+export const MenuButton = styled.button`
+
+`
+export const Header = styled.nav`
+background-color: white;
+color: #ee82ee;
+text-align: center;
+font-family: cursive;
+height: 30px;
+padding: 10px;
+
+
+`;
+
+export const Ul = styled.ul`
+ list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+
+`;
+
+export const Li = styled.li`
+    display: inline;
+    margin-right: 10px;
+`;
+
+export const A = styled.a`
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+
+  &:hover{
+    background-color: #111;
+ }
+
+`;
+
+export const ContentSearchHinos = styled.div`
+margin: 25%;
+margin-top: 8%;
+
+ @media (max-width: 768px) {
+    margin: 15%;
+    margin-top: 20%;
+  }
+
+`;
+export const ContentSearchListHinos = styled.div`
+margin: 10%;
+margin-top: 0;
+
+`
