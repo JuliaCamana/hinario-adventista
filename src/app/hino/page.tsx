5a44ee5de684887fe45fe55fca60b2568ad86efa
Julia Camana <juliacamana.2017@gmail.com>
2025-07-21 20:33:28 +0200

    com suspense

--- src/app/hino/page.tsx
+++ src/app/hino/page.tsx
@@ -1,92 +1,91 @@
 'use client'
-import React from "react";
+
+import React, { Suspense } from "react";
 import { FaMagnifyingGlassMinus, FaMagnifyingGlassPlus } from "react-icons/fa6";
 import { ButtonLeft, Content, ContentHino, ContentTitulo, P, PNoEstrofe, Rodape } from "./styles";
 import { FaHome, FaList } from "react-icons/fa";
 import { useRouter, useSearchParams } from "next/navigation";
 import hinos from "@/app/datas/letraHinos.json";
 
+export default function HinoPage() {
+  return (
+    <Suspense fallback={<div>Carregando hino...</div>}>
+      <Hino />
+    </Suspense>
+  );
+}
 
+function Hino() {
+  const router = useRouter();
+  const posicao = 0;
+  const searchParams = useSearchParams();
+  const no = searchParams.get('num');
 
+  const numero = Array.isArray(no) ? parseInt(no[0]) : parseInt(no ?? '');
+  const hino = hinos.find((hino) => hino.numero === numero);
 
-export default function Hino() {
+  const handleHome = () => {
+    router.push('/');
+  };
 
-    const router = useRouter();
-    const posicao = 0;
-    const searchParams = useSearchParams();
-    const no = searchParams.get('num');
+  return (
+    <div>
+      <ContentTitulo>
+        <h1>{hino?.titulo}</h1>
+      </ContentTitulo>
 
-    const numero = Array.isArray(no) ? parseInt(no[0]) : parseInt(no ?? '')
-    const hino = hinos.find((hino) => hino.numero === numero)
+      <Content>
+        <ButtonLeft>&lt;</ButtonLeft>
+        <ContentHino>
+          {hino?.temCoro ? hino.posicaoCoro === posicao ? (
+            <div>
+              <PNoEstrofe>Coro</PNoEstrofe>
+              {hino.coro?.map((verso, index) => (
+                <P key={index}>{verso}</P>
+              ))}
+            </div>
+          ) : (
+            <div>
+              {hino.estrofes.map((estrofe, i) => (
+                <div key={i}>
+                  <PNoEstrofe>{i + 1}</PNoEstrofe>
+                  {estrofe.map((verso, index) => (
+                    <P key={index}>{verso}</P>
+                  ))}
 
-    const handleHome = () => {
-        router.push('/');
-    }
-    return (
-        <div>
+                  {hino.posicaoCoro === i + 1 && (
+                    <div>
+                      <PNoEstrofe>Coro</PNoEstrofe>
+                      {hino.coro?.map((verso, index) => (
+                        <P key={index}>{verso}</P>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>
+              {hino?.estrofes.map((estrofe, index) => (
+                <div key={index}>
+                  <PNoEstrofe>{index + 1}</PNoEstrofe>
+                  {estrofe.map((verso, i) => (
+                    <P key={i}>{verso}</P>
+                  ))}
+                </div>
+              ))}
+            </div>
+          )}
+        </ContentHino>
+        <ButtonLeft>&gt;</ButtonLeft>
+      </Content>
 
-            <ContentTitulo>
-                <h1>{hino?.titulo}</h1>
-            </ContentTitulo>
-            <Content>
-                <ButtonLeft>&lt;</ButtonLeft>
-                <ContentHino>
-                    {hino?.temCoro ? hino.posicaoCoro === posicao ?
-                        <div>
-                            <PNoEstrofe>Coro</PNoEstrofe>
-                            {hino.coro?.map(
-                                (verso, index) => <P key={index} >{verso}</P>
-                            )}
-                        </div>
-                        : <div>
-                            {hino.estrofes.map((estrofe, i) => (
-                                <div key={i}>
-                                    <PNoEstrofe>{i + 1}</PNoEstrofe>
-                                    {estrofe.map(
-                                        (verso, index) => <P key={index}>{verso}</P>
-                                    )}
-                                
-                                {hino.posicaoCoro === i+1 && (
-                                <div>
-                                    <PNoEstrofe>Coro</PNoEstrofe>
-                                    {hino.coro?.map(
-                                        (verso, index) =>(<P key={index} >{verso}</P>)
-                                    )}
-                                </div>
-                                )}
-                            </div>
-                            ))
-                            }
-                        </div>
-                        : <div>
-                            {hino?.estrofes.map((estrofe, index) => (
-                                <div key={index}>
-                                    <PNoEstrofe>{index + 1}</PNoEstrofe>
-                                    {estrofe.map((verso, i) => (
-                                        <P key={i}>{verso}</P>
-                                    ))}
-                                </div>
-                            ))
-                            }
-                        </div>
-                    }
-
-                </ContentHino>
-                <ButtonLeft>&gt;</ButtonLeft>
-            </Content>
-            <Rodape>
-
-                <FaMagnifyingGlassPlus />
-                <FaMagnifyingGlassMinus />
-                <FaHome onClick={handleHome} />
-                <FaList />
-
-            </Rodape>
-
-
-        </div>
-    );
-
-
-
+      <Rodape>
+        <FaMagnifyingGlassPlus />
+        <FaMagnifyingGlassMinus />
+        <FaHome onClick={handleHome} />
+        <FaList />
+      </Rodape>
+    </div>
+  );
 }
